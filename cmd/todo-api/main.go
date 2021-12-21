789283fe6ca7eccdacb1c59417e9d0b2578e6a9b
mkaiho <mkaiho.cgengo@gmail.com>
2021-12-21 14:06:07 +0000

    Add gin library

--- cmd/todo-api/main.go
+++ cmd/todo-api/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mkaiho/go-todo-sample/util"
+)
 
 func main() {
+	router := gin.Default()
+
+	router.GET("/users/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		if util.IsEmptyString(id) {
+			statusCode := http.StatusBadRequest
+			c.JSON(statusCode, gin.H{
+				"message": fmt.Sprintf("%s: id=\"%v\"", http.StatusText(statusCode), id),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"id":   id,
+			"name": "test_user",
+		})
+	})
+	router.Run(":3000")
+
 	fmt.Println("Hello World!")
 }
